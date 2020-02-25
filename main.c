93889c40ab19692bb68881f7f6328ee3e19014dd
forrestak <35379892+forrestak@users.noreply.github.com>
2020-02-25 14:30:30 -0700

    Finished Intro Outline

--- main.c
+++ main.c
@@ -9,7 +9,20 @@
       - Slides up like in real game
       - Press Enter to Start?
     - Intro Text
-      -
-
-
+      - "Hi! Sorry to keep you waiting! Welcome to the world of Pokemon!
+        My name is Heather Marriot. But everyone calls me the Coding Professor.
+        This world is widely inhabited by creatures known as Pokemon.
+        We humans live alongside Pokemon, at times as friendly playmates,
+        and at times as cooperative workmates.
+        And sometimes, we band together and battle others like us.
+        But despite our closeness, we don't know everything about Pokemon.
+        In fact, there are many, many secrets surrounding Pokémon.
+        To unravel Pokemon mysteries, I've been undertaking research.
+        That's what I do. And you are?"
+      - Have user enter name <player>
+      -"Ah, okay! You're <player> who's moving to my hometown of Prescott.
+        I get it now! All right, are you ready? Your very own adventure is about
+        to unfold. Take courage, and leap into the world of Pokemon where
+        dreams, adventure and friendships await! First, let's choose a
+        Pokemon companion!"
 */

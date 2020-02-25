ae3aa04702ea24b2d3b389f48b20782c7df490f0
forrestak <35379892+forrestak@users.noreply.github.com>
2020-02-25 14:21:38 -0700

    Added notes to main.c

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
 /*  CS125_Pokemon
     CS125 Programming Project
 */
+
+/* Intro
+    - Title Screen
+      - Pokemon
+      - Title of game (Eagle, Embry - Riddle)
+      - Slides up like in real game
+      - Press Enter to Start?
+    - Intro Text
+      -
+
+
+*/

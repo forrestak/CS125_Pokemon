275f61f45497307a5433bf5e10f77f822baa1d24
forrestak <35379892+forrestak@users.noreply.github.com>
2020-02-26 15:48:05 -0700

    Got Pokemon to print like a printer

--- intro.c
+++ intro.c
@@ -8,20 +8,35 @@
 */
 
 #include <stdio.h>
-#include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 void intro(){
+  //Printing the Pokemon Logo
+  printf("\n");
   printf("                                     ,\'\\\n");
+  sleep(1);
   printf("       _.----.        ____         ,'  _\\   ___    ___     ____\n");
+  sleep(1);
   printf("   _,-'       `.     |    |  /`.   \\,-'    |   \\  /   |   |    \\  |`.\n");
+  sleep(1);
   printf("   \\      __    \\    '-.  | /   `.  ___    |    \\/    |   '-.   \\ |  |\n");
+  sleep(1);
   printf("    \\.    \\ \\   |  __  |  |/    ,','_  `.  |          | __  |    \\|  |\n");
+  sleep(1);
   printf("      \\    \\/   /,' _`.|      ,' / / / /   |          ,' _`.|     |  |\n");
+  sleep(1);
   printf("       \\     ,-'/  /   \\    ,'   | \\/ / ,`.|         /  /   \\  |     |\n");
+  sleep(1);
   printf("        \\    \\ |   \\_/  |   `-.  \\    `'  /|  |    ||   \\_/  | |\\    |\n");
+  sleep(1);
   printf("         \\    \\ \\      /       `-.`.___,-' |  |\\  /| \\      /  | |   |\n");
+  sleep(1);
   printf("          \\    \\ `.__,'|  |`-._    `|      |__| \\/ |  `.__,'|  | |   |\n");
+  sleep(1);
   printf("           \\_.-'       |__|    `-._ |              '-.|     '-.| |   |\n");
+  sleep(1);
   printf("                                   `'                            '-._|\n");
+
+
 }

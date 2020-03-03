bc191dc1ce1604d15558cdb08934175f92ef4f85
YawnInfection <61335011+yawninfection@users.noreply.github.com>
2020-03-03 14:24:25 -0700

    Update BattleUI.c

--- BattleUI.c
+++ BattleUI.c
@@ -26,36 +26,10 @@ void run(char screen[ROWS][COLS])
     for(x=0;x<ROWS;x++)
     printf("%s\n",screen[x]);
 }
-///////////////////////////////////////
-void changeEnemy(char screen[ROWS][COLS],int pokemonIndex)
-{
-
-char enemyPokemon[17][31];
-
-
-
-
-
-
-char spare[COLS-30];
-strncpy(spare,screen[0],COLS-31);
-
-char spare2[COLS];
-strcat(spare2,spare);
-//strcat(spare2, image[0]);
-printf("%s",spare2);
-
-printf("\nhere\n");
-
-}
-//////////////////////////////////////
-char* getPokemon(int pokemonIndex,int lineNumber){
+///////////////////////////////////
+void getPokemon(char holder[17][31],int pokemonIndex){
 int increment=0;
 char pokemon[17][31];
-char returnStatement[31]={};
-//you cant really return whole arrays, but you can return whole strings
-//so do like a for loop to get all of the lines of a bulbasaur out there :D
-//pokemonIndex is probably for choosing the pokemon
 char bulbasaur[17][31]=
 {
   "                              ",
@@ -75,66 +49,99 @@ char bulbasaur[17][31]=
   "    `+++++. .+so+:l+:l+++++l  ",
   "     -.--`   `::ll:`  -:::-   ",};
 
+//https://stackoverflow.com/questions/12675800/how-to-copy-matrix-in-c
+memcpy(holder,bulbasaur,sizeof(bulbasaur));
 
-
-printf("location 1\n");
-for(increment;increment<17;increment++)
+}
+///////////////////////////////////////
+void changeEnemy(char screen[ROWS][COLS],int pokemonIndex)
 {
-//printf("%d",increment);
-strncpy(pokemon[increment],bulbasaur[increment],31);
-printf("%s\n",pokemon[increment]);
+int count;
+char enemyPokemon[17][31];
+char cleanSpare[COLS-30];
+char spare[COLS-30];
+char completeSpare[COLS];
+char cleanCompleteSpare[COLS];
+
+getPokemon(enemyPokemon,4);
+
+//printf("ur here \n%s\n%s\n%s\n%s\n",enemyPokemon[4],enemyPokemon[5],enemyPokemon[6],enemyPokemon[7]);
+
+//strncpy(spare,putty,30);
+//strncpy(completeSpare,screen[0],COLS-31);
+//strcat(completeSpare,spare);
+//strncpy(screen[0],completeSpare,COLS);
+
+
+for(count=0;count<17;count++)
+{
+memcpy(spare,cleanSpare,sizeof(spare));
+memcpy(completeSpare,cleanCompleteSpare,sizeof(completeSpare));
+strncpy(spare,enemyPokemon[count],30);
+strncpy(completeSpare,screen[count],COLS-31);
+strcat(completeSpare,spare);
+strncpy(screen[count],completeSpare,COLS);
+
+
 }
 
-printf("location 2\n");
 
 
-printf("you are here\n");
-printf("%s\n",pokemon[4]);
+//strncpy(spare,screen[0],COLS-31);
 
-printf("and here too\n");
+//printf("%s",completeSpare);
 
-
-strncpy(returnStatement,pokemon[5],31);
-
-printf("location 3\n");
-
-
-printf("%s\n",pokemon[5]);
-printf("location 4\n");
-printf("%s\n",returnStatement);
-
-printf("location 5\n");
-
-//printf("^^^^^^^^^^^^^^%s^^^^^^^^^^^^^^^\n",returnStatement);
-return "sup bruh";
+//printf("\nhere\n");
 
 }
+//////////////////////////////////////
+
 
 
 /////////////////////////////////////
 /////////////////////////////////////
 /*
-
+                
 //https://www.text-image.com/convert/ascii.html
+//30 width
 //use this website cause theres no asterisks
 //also replace all slashes with l
 
-                      .l::`
-            .--:lllllll+loo`
-    ..`   `+llllo+llllllls+
-   l::l::l++llll:++lllll+sol
-  :l--loss+l:::::lo+++oooosol
+                      .l::`   
+            .--:lllllll+loo`  
+    ..`   `+llllo+llllllls+   
+   l::l::l++llll:++lllll+sol  
+  :l--loss+l:::::lo+++oooosol 
  -lllo:lll:l:l+l:+oooooooosoo:
 .:+ll:loo:l:.+:o+++osssoooooo+
 l-ol:::ll::: sloo+++oyysysoool
-:+ll::l::::l:l+oo++++ossoosol
- `-+soo++++osoo+++lll+++l++:
-   .l+oooosooooo:++:lo+oso++`
-    +os+++o+oss+oll:+oloso+o`
-    `+++++. .+so+:l+:l+++++l
-     -.--`   `::ll:`  -:::-
+:+ll::l::::l:l+oo++++ossoosol 
+ `-+soo++++osoo+++lll+++l++:  
+   .l+oooosooooo:++:lo+oso++` 
+    +os+++o+oss+oll:+oloso+o` 
+    `+++++. .+so+:l+:l+++++l  
+     -.--`   `::ll:`  -:::-   
 
 
+
+https://bulbapedia.bulbagarden.net/wiki/File:Spr_b_g1_001.png
+so bulbapedia, for the backs,gen 1, blue
+
+                              
+                              
+                              
+                              
+                              
+        +                     
+      :h ++                   
+      N:h  yyN+o+:            
+      N:Ns:. .+:+:y:+   ++    
+    oNh.yhs::.    yy.+o+  N   
+  ohhh: :Ns+:::.   .h:N .y+   
+ N+hhs:  sNs+:::    N:N:yooN  
+ N.Nss:  :hhs+:::..:NNh+sNNho 
+ N.hhss+. +hhss+++hNNs+hhhhs+ 
+  +y+hhhhhoyyNhhNNNhss++h+    
 
 
 */
@@ -145,7 +152,7 @@ int main()
 {
 printf("\e[2J\e[H");
 //32 rows, 64 columns
-char screen[ROWS][COLS] ={
+char screen[ROWS][COLS] ={   
 "0123456789012345678901234567890123456789012345678901234567890123",
 "0123456789012345678901234567890123456789012345678901234567890123",
 "0123456789012345678901234567890123456789012345678901234567890123",
@@ -203,7 +210,6 @@ char test[17][31]=
 
 
 
-getPokemon(3,3);
 
 
 ////////////////

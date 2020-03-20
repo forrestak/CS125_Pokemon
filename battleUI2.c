0b914800ae3070660f3d83849ff7600bcc0635ff
YawnInfection <61335011+yawninfection@users.noreply.github.com>
2020-03-20 15:07:28 -0700

    Update battleUI2.c

--- battleUI2.c
+++ battleUI2.c
@@ -10,126 +10,153 @@
 #include <unistd.h>
 #include "pokeFiles.c"
 
+
 void getPokemon(char holder[17][31],int pokemonIndex);
+//get pokemon is fully defined in pokefiles btw
+void delay(int milliseconds)
+{
+    milliseconds*=1000;
+    clock_t start_time =clock();
+    while(clock()<start_time + milliseconds);
+}
+///////////////////////////////////
+void clear()
+{
+    printf("\e[2J\e[H");
+}
+//////////////////////////////////
+void run(char screen[33][65])
+{
+    int x=0;
+    for(x=0;x<33;x++)
+    printf("%s\n",screen[x]);
+}
+///////////////////////////////////////
+void changeText(char screen[33][65],int xScreenPos,int yScreenPos,char sentence[])
+{
+    char spareLine[65];
+    char secondSpareLine[65];
+    char completeSpareLine[65];
 
-void delay(int milliseconds);
-void clear();
-void run(char screen[33][65]);
-void changeEnemy(char screen[33][65],int pokemonIndex);
-//#define ROWS 33
-//#define COLS 65
+    strncpy(spareLine,screen[yScreenPos],xScreenPos);
+    strncpy(secondSpareLine,screen[yScreenPos]+xScreenPos+strlen(sentence),65);
+    strcat(completeSpareLine,spareLine);
+    strcat(completeSpareLine,sentence);
+    strcat(completeSpareLine,secondSpareLine);
+    strncpy(screen[yScreenPos],completeSpareLine,65);
 
-void battleUI(){
-  clear();
-  //32 rows, 64 columns
-  char screen[33][65] =
-  {
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123",
-    "0123456789012345678901234567890123456789012345678901234567890123"
-  };
+}
 
-  //test 16 lines height
-  //30 char width
-  char test[17][31]=
-  {
-    "                              ",
-    "                              ",
-    "                      .l::`   ",
-    "            .--:lllllll+loo`  ",
-    "    ..`   `+llllo+llllllls+   ",
-    "   l::l::l++llll:++lllll+sol  ",
-    "  :l--loss+l:::::lo+++oooosol ",
-    " -lllo:lll:l:l+l:+oooooooosoo:",
-    ".:+ll:loo:l:.+:o+++osssoooooo+",
-    "l-ol:::ll::: sloo+++oyysysoool",
-    ":+ll::l::::l:l+oo++++ossoosol ",
-    " `-+soo++++osoo+++lll+++l++:  ",
-    "   .l+oooosooooo:++:lo+oso++` ",
-    "    +os+++o+oss+oll:+oloso+o` ",
-    "    `+++++. .+so+:l+:l+++++l  ",
-    "     -.--`   `::ll:`  -:::-   ",
-  };
 
-  int ice;
-  for(ice=1;ice<=151;ice++){
-    changeEnemy(screen,ice);
+///////////////////////////////////////
+void changeEnemyPokemon(char screen[33][65],int pokemonIndex)
+{
+    int count;
+    char enemyPokemon[17][31];
+    char cleanSpare[65-30];
+    char spare[65-30];
+    char completeSpare[65];
+    char cleanCompleteSpare[65];
+    getPokemon(enemyPokemon,pokemonIndex);
+    for(count=0;count<16;count++)
+    {
+        memcpy(spare,cleanSpare,sizeof(spare));
+        memcpy(completeSpare,cleanCompleteSpare,sizeof(completeSpare));
+        strncpy(spare,enemyPokemon[count],30);
+        strncpy(completeSpare,screen[count],34);
+        strcat(completeSpare,spare);
+        strncpy(screen[count],completeSpare,64);
+    }    
+}
+
+///////////////////////////////////////
+void changePlayerPokemon(char screen[33][65],int pokemonIndex)
+{
+
+//screen[33]="0123456789012345678901234567890123456789012345678901234567890123";
+
+
+    int count;
+    int shift=16;//how down
+    char enemyPokemon[17][31];
+    char cleanSpare[65-30];
+    char spare[65-30];
+    char completeSpare[65];
+    char cleanCompleteSpare[65];
+    char dummy[10]="hellodummy";
+    getPokemon(enemyPokemon,pokemonIndex);
+    for(count=shift+0;count<16+shift;count++)
+    {
+        memcpy(spare,cleanSpare,sizeof(spare));
+        memcpy(completeSpare,cleanCompleteSpare,sizeof(completeSpare));
+        strncpy(completeSpare,enemyPokemon[count-shift],31);
+        strncpy(spare,screen[count]+30,65-31);
+        strcat(completeSpare,spare);
+        strncpy(screen[count],completeSpare,64);
+    }
+}
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+///////////////////////////////////////
+void battleUI()
+{
+clear();
+//32 rows, 64 columns
+char screen[33][65] ={   
+"+--------------------------------+-----------------------------+",
+"|insert name here6789012345678901|23456789012345678901234567890|",
+"|graphical health6789012345678901|23456789012345678901234567890|",
+"|numerical stuff56789012345678901|23456789012345678901234567890|",
+"|graphical exp3456789012345678901|23456789012345678901234567890|",
+"+--------------------------------+23456789012345678901234567890|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"                                  34567890123456789012345678901|",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678                                  ",
+"|01234567890123456789012345678+--------------------------------+",
+"|01234567890123456789012345678|insert name here6789012345678901|",
+"|01234567890123456789012345678|graphical hpalth6789012345678901|",
+"|01234567890123456789012345678|numerical stuffh6789012345678901|",
+"|01234567890123456789012345678|graphical exp3456789012345678901|",
+"+-----------------------------+--------------------------------+",};
+//bot left pokemon 30 width
+//top right pokemon 32 width? wtf
+//test 16 lines height
+//30 char width
+
+changeText(screen,1,1,"not pikachu");
+
+int ice;
+for(ice=1;ice<=151;ice++)
+{
+    changeEnemyPokemon(screen,ice);
+    changePlayerPokemon(screen,152-ice);
     run(screen);
-    delay(500);
+    delay(100);
     clear();
-  }
-
-  int i;
-  for(i=0;i<10;i++){
-    delay(500);
-    run(screen);
-    clear();
-  }
-
 }
 
-//https://www.geeksforgeeks.org/time-delay-c/
-void delay(int milliseconds){
-  milliseconds*=1000;
-  clock_t start_time =clock();
-  while(clock()<start_time + milliseconds);
-}
+//return 0;
 
-void clear(){
-  printf("\e[2J\e[H");
-}
 
-void run(char screen[33][65]){
-  int x=0;
-  for(x=0;x<33;x++)
-  printf("%s\n",screen[x]);
-}
-
-void changeEnemy(char screen[33][65],int pokemonIndex){
-  int count;
-  char enemyPokemon[17][31];
-  char cleanSpare[65-30];
-  char spare[65-30];
-  char completeSpare[65];
-  char cleanCompleteSpare[65];
-
-  getPokemon(enemyPokemon,pokemonIndex);
-
-  for(count=0;count<16;count++){
-    memcpy(spare,cleanSpare,sizeof(spare));
-    memcpy(completeSpare,cleanCompleteSpare,sizeof(completeSpare));
-    strncpy(spare,enemyPokemon[count],30);
-    strncpy(completeSpare,screen[count],65-31);
-    strcat(completeSpare,spare);
-    strncpy(screen[count],completeSpare,65);
-  }
 }

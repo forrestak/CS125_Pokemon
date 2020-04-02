027b312d2a4b29172e2c88f0266062494179b909
forrestak <35379892+forrestak@users.noreply.github.com>
2020-04-01 20:47:15 -0700

    updates again

--- endSim.c
+++ endSim.c
@@ -21,6 +21,7 @@ void endGame(int win){
   // Win Scenario
   if (win == 1){
     char text1[] = "Congratulations! You have completed the tutorial simulator! You win!\n";
+    char text2[] = "This is the end of the tutorial simulator. I hope you enjoyed it\nand learned the basics for your upcoming adventure. I look forward to\nhearing all of the amazing stories about you and your companions!\nSee ya!\n"
     readPrint(count, text1, "standard");
     // Printing Trophy
     printf("\n");
@@ -28,13 +29,17 @@ void endGame(int win){
     asciiart(6);
     printf("\n");
     sleep(1);
+    readPrint(count, text2, "standard");
   }
   // Lose Scenario
   else if (win == 0){
     char text1[] = "Aww! That's too bad! You gave a good show though! I'm sure\nif you try again you'll win next time!\n";
-
     readPrint(count, text1, "standard");
-    
+    printf("\n");
+    sleep(1);
+    asciiart(7);
+    printf("\n");
+    sleep(1);
   }
 
 }

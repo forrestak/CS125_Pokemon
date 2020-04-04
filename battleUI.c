6b9af16a575c61d87f2237bb77a157948ccea580
YawnInfection <61335011+yawninfection@users.noreply.github.com>
2020-04-03 20:36:31 -0400

    Update battleUI.c

--- battleUI.c
+++ battleUI.c
@@ -69,18 +69,34 @@ void run(char screen[33][65], char colorScreen[33][65])
 ///////////////////////////////////////
 void changeText(char screen[33][65],int xScreenPos,int yScreenPos,char sentence[])
 {
-    char spareLine[65];
-    char secondSpareLine[65];
-    char completeSpareLine[65];
-
+    fflush(stdout);
+    char spareLine[65]="";
+    char secondSpareLine[65]="";
+    char completeSpareLine[65]="";
+    //literally had an issue because i didnt intialize the chars with an empty string
+    //printf("screenq%s\n",screen[yScreenPos]);
     strncpy(spareLine,screen[yScreenPos],xScreenPos);
+    //printf("spare q%s\n",spareLine);
     strncpy(secondSpareLine,screen[yScreenPos]+xScreenPos+strlen(sentence),65);
-    strcat(completeSpareLine,spareLine);
+    //printf("2ndsreq%s\n",secondSpareLine);
+    //next three functions are to put together the final result
+    strncpy(completeSpareLine,spareLine,strlen(spareLine));
+    //printf("final1q%s\n",completeSpareLine);
     strcat(completeSpareLine,sentence);
+    //printf("final2q%s\n",completeSpareLine);
     strcat(completeSpareLine,secondSpareLine);
+    //printf("final3q%s\n",completeSpareLine);
+    //copy the final result
     strncpy(screen[yScreenPos],completeSpareLine,65);
+    //printf("last  q%s\n",screen[yScreenPos]);
+
 
 }
+void changeColor(char colorScreen[33][65],int x, int y,char input)
+{
+    colorScreen[x][y]=input;
+}
+//^^^^^^^^^^^^^^^^^^more changes on 4/3/2020 evening
 
 ///////////////////////////////////////
 void changeEnemyPokemon(char screen[33][65],int pokemonIndex)
@@ -201,22 +217,26 @@ void battleUI()
 "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
 "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
 "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
-"wwwwwwwwwwwwwwwrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrwwww",
 "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
 "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
 "wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
-"wwwwwwwwwwwwwwwrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrwwww",
-"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwgggggggggbbbbbgbgbgwwww",
-"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwpppppppppppwwwwwyyyywwwww",};
+"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",
+"wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww",};
     //bot left pokemon 30 width
     //top right pokemon 32 width? wtf
     //test 16 lines height
     //30 char width
 
-    changeText(screen,1,1,"not pikachu");
+    //changeText(screen,1,1,"a");
     char testInput[100];
     int testInputInt=1;
     int ice;
+    //changeText(screen,3,3,"wheee");
+    //changeText(screen,0,0,"asparagus");
+    //changeText(screen,8,3,"waffles");
+    //changeColor(colorScreen,1,1,'b');
     for(ice=1;ice<=151;ice++)
     {
       //printf("here%d\n",testInputInt);
@@ -224,6 +244,7 @@ void battleUI()
       //    changeEnemyPokemon(screen,ice);
       //    changePlayerPokemon(screen,152-ice);
       changePlayerPokemon(screen,152-testInputInt);
+
       run(screen,colorScreen);
       delay(100);
       scanf("%s",testInput);
